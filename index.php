d20f1346a8992838a80d15959547b276420d47ce
hugoamiach <hugo.amiach@hotmail.fr>
2019-01-08 12:19:03 +0100

    struct html, redirection de controller

--- index.php
+++ index.php
@@ -1,36 +1,36 @@
 <?php
 session_start();
 include "lib/Database.php";
-/*require "V/map.php";*/
 
-if(!isset($_POST["controller"]) && empty($_POST['controller']) && !isset($_GET["controller"]) && empty($_GET["controller"])){
+if (!isset($_POST["controller"]) && empty($_POST['controller']) && !isset($_GET["controller"]) && empty($_GET["controller"])) {
 	$_POST['controller'] = "Utilisateur";
-	$_POST['f'] = "verifConnection"; 
+	$_POST['f'] = "verifConnection";
 }
 
-if(isset($_POST["controller"]) && !empty($_POST['controller'])){
-	if(file_exists("C/".$_POST["controller"] . ".php")){
-		require "C/".$_POST["controller"] . ".php";
-		if(function_exists ($_POST["f"])){
+if (isset($_POST["controller"]) && !empty($_POST['controller'])) {
+	if (file_exists("C/" . $_POST["controller"] . ".php")) {
+		require "C/" . $_POST["controller"] . ".php";
+		if (function_exists($_POST["f"])) {
 			$_POST["f"]($_GET, $_POST);
-		}else{
+		} else {
 			echo "probleme fonction";
-		}	
-	}else{
+		}
+	} else {
 		echo "probleme controller";
 	}
 }
 
-if(isset($_GET["controller"]) && !empty($_GET["controller"])){
-	if(file_exists("C/".$_GET["controller"] . ".php")){
-		require "C/".$_GET["controller"] . ".php";
-		if(function_exists ($_GET["f"])){
+if (isset($_GET["controller"]) && !empty($_GET["controller"])) {
+	if (file_exists("C/" . $_GET["controller"] . ".php")) {
+		require "C/" . $_GET["controller"] . ".php";
+		if (function_exists($_GET["f"])) {
 			$_GET["f"]($_GET, $_POST);
-		}else{
+		} else {
 			echo "probleme fonction";
-		}	
-	}else{
+		}
+	} else {
 		echo "probleme controller";
-	}	
+	}
 }
 
+var_dump($_POST);
---
+++ lib/header.php
@@ -0,0 +1,24 @@
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Map</title>
+
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+
+
+    <!-- <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.1/leaflet.css" /> -->
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+
+    <!-- <script src="http://cdn.leafletjs.com/leaflet-0.7.1/leaflet.js"></script> -->
+
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
