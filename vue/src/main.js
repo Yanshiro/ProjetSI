23e5cc198ba68cba3f3e2f965938916ac0646f50
hugoamiach <hugo.amiach@hotmail.fr>
2019-05-22 14:14:04 +0200

    adaptation

--- vue/src/main.js
+++ vue/src/main.js
@@ -36,7 +36,7 @@ const router = new VueRouter({
 })
 
 
-Vue.prototype.$urlApi = "http://127.0.0.1/addsite/ProjetSI/ProjetSIGit/"
+Vue.prototype.$urlApi = "http://127.0.0.1/Projetsi/"
 Vue.prototype.$getTypeChampsInputBySQL = function (typeSql) {
   var int = /[int]/;
   if (typeSql.match(int)) {
